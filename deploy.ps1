28045fe14933c63f92d13d937843ab7b6fb2293f
schwamster <bastian.toepfer@gmail.com>
2018-01-24 08:47:01 +0100

    updated to cloudfront with https

--- deploy.ps1
+++ deploy.ps1
@@ -2,5 +2,5 @@ cd ./infrastructure
 terraform apply -var 'domain=greenelephant.io.' -var 'subdomain=kidsgame'
 cd ..
 
-yarn build-local
-aws s3 sync --delete --acl public-read .\dist\ s3://kidsgame.greenelephant.io
+# yarn build-local
+# aws s3 sync --delete --acl public-read .\dist\ s3://kidsgame.greenelephant.io
