679539d4626f4a0970d317abe9cfc041a03b4890
schwamster <bastian.toepfer@gmail.com>
2018-10-13 21:32:05 +0200

    iopipe integration

--- kidsgame-api/progress.ts
+++ kidsgame-api/progress.ts
@@ -1,9 +1,11 @@
+const iopipe: any = require('@iopipe/iopipe')();
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
 import { DynamoDB } from 'aws-sdk';
 
 const docClient = new DynamoDB.DocumentClient({ region: 'eu-west-1' });
 
-export const getProgress: Handler = (event: any, context: Context, cb: Callback) => {
+
+export const getProgress: Handler = iopipe((event: any, context: Context, cb: Callback) => {
 
   console.log("event", event);
   var dynamodb = new DynamoDB();
@@ -22,25 +24,26 @@ export const getProgress: Handler = (event: any, context: Context, cb: Callback)
     }
     else {
       console.log("data:", data);
-      cb(null, 
-      { 
-        statusCode: 200, 
-        headers: {
-          "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-          "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
-        },
-        body: JSON.stringify({
-          data: data.Item
-        })
-      });
+      cb(null,
+        {
+          statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS 
+          },
+          body: JSON.stringify({
+            data: data.Item
+          })
+        });
     }
   });
-}
+})
 
 
-export const addProgress: Handler = (event: any, context: Context, cb: Callback) => {
+export const addProgress: Handler = iopipe((event: any, context: any, cb: Callback) => {
 
   console.log("event", event);
+  const { start, end } = context.iopipe.mark;
   const eventData: any = JSON.parse(event.body);
   let dynamodb = new DynamoDB();
 
@@ -51,22 +54,27 @@ export const addProgress: Handler = (event: any, context: Context, cb: Callback)
     TableName: process.env.DYNAMODB_TABLE
   };
 
+  start('getProgress-DynamoDb');
   var item = docClient.get(params, function (err, data) {
+    end('getProgress-DynamoDb');
     if (err) {
       console.log(err, err.stack);
       cb(err, getResponse(500, "An error occured"));
     }
     else {
       console.log("data:", data);
-      let currentScore = data.Item? data.Item.points : 0;
+      let currentScore = data.Item ? data.Item.points : 0;
       let newScore = currentScore + eventData.points;
+      start('putProgress-DynamoDb');
       PutProgress(event.pathParameters.userId, newScore, cb);
-      cb(null, 
-        { 
-          statusCode: 200, 
+      end('putProgress-DynamoDb');
+      context.iopipe.label('supidupi');
+      cb(null,
+        {
+          statusCode: 200,
           headers: {
-            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-            "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS 
           },
           body: JSON.stringify({
             points: newScore
@@ -74,7 +82,7 @@ export const addProgress: Handler = (event: any, context: Context, cb: Callback)
         });
     }
   });
-}
+})
 
 function PutProgress(userId: string, points: number, callback: Callback) {
   var params = {
@@ -102,8 +110,8 @@ function getResponse(statusCode: number, message: string) {
   var response = {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-      "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS 
     },
     body: JSON.stringify({
       message: 'Ok'
